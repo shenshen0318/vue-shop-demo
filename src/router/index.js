28d05b94ad431834787da1ff14def834cbf2b99d
mac <shenshen0318@gmail.com>
2021-04-15 23:42:06 +0800

    完善登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import VueRouter from 'vue-router'
 
 // 导入组件
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -19,12 +20,29 @@ const routes = [
   {
     path: '/login',
     component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 // 创建路由实例
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next)=>{
+  // to 将要访问的路径
+  // from 从哪个路径跳转而来
+  // next 一个函数 表示放行 next()  或者  next('路径') 
+  if(to.path === '/login') return next();
+  // 获取 token
+  const token = window.sessionStorage.getItem("token");
+  // 没有 token 强制跳转到登录页面
+  if(!token) return next('/login')
+  next();
 })
 
 // 导出
